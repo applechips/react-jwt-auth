8b7a0670f66ef97000f9a9d2fa56e924ccb91e5c
Nicole <nhswong@gmail.com>
2017-02-02 10:35:37 -0800

    add express middleware + nodemon

--- server/index.js
+++ server/index.js
@@ -6,11 +6,11 @@ const morgan = require('morgan');
 const app = express();
 
 // App Setup - get express working
+app.use(morgan('combined'));
+app.user(bodyParser.json({ type: '*/*' }));
 
 // Server Setup - get express talking to the outside world
-
 const port = process.env.PORT || 3090;
-
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server listening on:', port );
